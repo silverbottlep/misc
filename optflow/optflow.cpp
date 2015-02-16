25d0b7194504819ef8b2d90ba1e8031b27f763d6
Eunbyung Park <silverbottlep@gmail.com>
2015-02-16 10:08:10 -0500

    compute optical flow for ucf101 dataset. optflow will be saved as .jpg files

--- optflow/optflow.cpp
+++ optflow/optflow.cpp
@@ -3,49 +3,113 @@
 #include <highgui.h>
 #include <opencv2/gpu/gpu.hpp>
 #include <iostream>
+#include <fstream>
 
 using namespace cv;
+using namespace std;
+
+#define MAX_FRAMES 2000
+
+string dir = "/home/eunbyung/Works/data/ucf101_jpeg/";
+string outdir = "/home/eunbyung/Works/data/ucf101_opt_jpeg/";
+
 
 int main(int argc, char** argv )
 {
     if ( argc != 3 )
     {
-        printf("usage: optflow <source1 image> <source2 image>\n");
+        printf("usage: optflow <filelist> <nframes list>\n");
         return -1;
     }
 
-		cv::gpu::GpuMat gpu_src1;
-		cv::gpu::GpuMat gpu_src2;
-		cv::gpu::GpuMat gpu_flow_x;
-		cv::gpu::GpuMat gpu_flow_y;
-		cv::Mat src1_gray;
-		cv::Mat src2_gray;
+	std::ifstream filelist;
+	std::ifstream nframeslist;
 
-		cv::Mat src1 = cv::imread("src1.bmp",CV_LOAD_IMAGE_COLOR);
-		cv::Mat src2 = cv::imread("src2.bmp",CV_LOAD_IMAGE_COLOR);
 
-		cvtColor(src1, src1_gray, CV_BGR2GRAY);
-		cvtColor(src2, src2_gray, CV_BGR2GRAY);
+	filelist.open(argv[1],std::ifstream::in);
+	nframeslist.open(argv[2],std::ifstream::in);
+	int count = 0;
+	for( std::string fname; getline(filelist, fname); )
+	{
+		string nframes;
+		getline(nframeslist, nframes);
+		cv::Mat image[MAX_FRAMES];
+		cv::gpu::GpuMat gpu_src[MAX_FRAMES];
+		cv::gpu::GpuMat gpu_flow_x[MAX_FRAMES];
+		cv::gpu::GpuMat gpu_flow_y[MAX_FRAMES];
+		cv::Mat flow_x[MAX_FRAMES];
+		cv::Mat flow_y[MAX_FRAMES];
+		cv::Mat flow_x_8u[MAX_FRAMES];
+		cv::Mat flow_y_8u[MAX_FRAMES];
+		cv::Scalar avg_x;
+		cv::Scalar avg_y;
+		
 
-		gpu_src1.upload(src1_gray);
-		gpu_src2.upload(src2_gray);
+		// load image for one avi file, which is usually around 100 frames
+		int N = atoi(nframes.c_str());
+		for (int i=0; i<N; i++) {
+			char str[6]={0};
+			sprintf(str, "%04u", i+1);	
+			string f1 = dir+fname+"_f"+str+".jpg";
+			//cout << f1 << endl;
 
-		cv::gpu::BroxOpticalFlow OpticalFlowGPU = cv::gpu::BroxOpticalFlow(0.197f, 0.8f, 50.0f, 10, 77, 10);
-		OpticalFlowGPU(gpu_src1,gpu_src2,gpu_flow_x,gpu_flow_y);
+			image[i] = cv::imread(f1,CV_LOAD_IMAGE_GRAYSCALE);
+			image[i].convertTo(image[i], CV_32FC1);
+		}
 
-		cv::Mat flow_x;
-		cv::Mat flow_y;
-		gpu_flow_x.download(flow_x);
-		gpu_flow_y.download(flow_y);
+		// compute optical flow(mean substraction)
+		for (int i=0; i<N-1; i++) {
+			gpu_src[i].upload(image[i]);
+			gpu_src[i+1].upload(image[i+1]);
 
-    imshow("Display Image", src1_gray);
-    waitKey(0);
-    imshow("Display Image", src2_gray);
-    waitKey(0);
-    imshow("Display Image", flow_x);
-    waitKey(0);
-    imshow("Display Image", flow_y);
-    waitKey(0);
+			cv::gpu::BroxOpticalFlow OpticalFlowGPU = 
+				cv::gpu::BroxOpticalFlow(0.197f, 50.0f, 0.8f, 10, 77, 10);
+			OpticalFlowGPU(gpu_src[i],gpu_src[i+1],gpu_flow_x[i],gpu_flow_y[i]);
+			gpu_flow_x[i].download(flow_x[i]);
+			gpu_flow_y[i].download(flow_y[i]);
+			
+			avg_x = cv::mean(flow_x[i]);
+			avg_y = cv::mean(flow_y[i]);
+			flow_x[i] = flow_x[i] - avg_x;
+			flow_y[i] = flow_y[i] - avg_y;
+		}
+
+		// compute min and max for later scaling
+		double min=1000000, max=-1000000;
+		for (int i=0; i<N-1; i++) {
+			double loc_x_min, loc_x_max;
+			double loc_y_min, loc_y_max;
+
+			cv::minMaxIdx(flow_x[i], &loc_x_min, &loc_x_max);
+			cv::minMaxIdx(flow_y[i], &loc_y_min, &loc_y_max);
+
+			if (min > loc_x_min)
+				min = loc_x_min;
+			if (min > loc_y_min)
+				min = loc_y_min;
+			if (max < loc_x_max)
+				max = loc_x_max;
+			if (max < loc_y_max)
+				max = loc_y_max;
+		}
+		double scale = 255/(max-min);
+
+		// scaling into [0 255]
+		for (int i=0; i<N-1; i++) {
+			char str[6]={0};
+			sprintf(str, "%04u", i+1);	
+			string f1 = outdir+fname+"_f"+str+"_optx.jpg";
+			string f2 = outdir+fname+"_f"+str+"_opty.jpg";
+			
+			flow_x[i].convertTo(flow_x_8u[i], CV_8UC1, scale, -min*scale);
+			flow_y[i].convertTo(flow_y_8u[i], CV_8UC1, scale, -min*scale);
+
+			imwrite(f1, flow_x_8u[i]);
+			imwrite(f2, flow_y_8u[i]);
+		}
+		
+		cout << "count: " << ++count << " " << fname << endl;
+	}
 
     return 0;
 }
