b86fcdbf1678f58fed5fcbf9713a40b35361c613
Eunbyung Park <silverbottlep@gmail.com>
2015-02-28 14:47:09 -0500

    add ucfjpg_dir.m

---
+++ optflow/ucfjpg_dir.m
@@ -0,0 +1,25 @@
+src_dir = '/home/eunbyung/Works/data/ucf101_jpeg/';
+
+fid_fname = fopen('srclist_fnames.txt');
+fid_nframes = fopen('srclist_nframes.txt');
+
+fname_line = fgetl(fid_fname);
+nframe_line = fgetl(fid_nframes);
+while ischar(fname_line)
+  nframe = str2num(nframe_line);
+  filelist=[];
+  for i=1:nframe
+    filelist = [filelist ' ' strcat(fname_line,'_f',num2str(i,'%04u'),'.jpg ')];
+  end
+
+  mkdir(fullfile(src_dir,fname_line));
+  command = ['mv ' filelist ' ' fname_line '/'];
+  [status, cmdout] = system(command);
+  fprintf('%s: status(%d)\n', fname_line, status);
+
+  fname_line = fgetl(fid_fname);
+  nframe_line= fgetl(fid_nframes);
+end
+
+fclose(fid_fname);
+fclose(fid_nframes);
