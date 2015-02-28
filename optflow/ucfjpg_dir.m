19943ba140d4f61038306cd66d20466b1cbe9bfd
Eunbyung Park <silverbottlep@gmail.com>
2015-02-28 16:14:35 -0500

    fix bug of ucfjpg_dir.m

--- optflow/ucfjpg_dir.m
+++ optflow/ucfjpg_dir.m
@@ -9,11 +9,11 @@ while ischar(fname_line)
   nframe = str2num(nframe_line);
   filelist=[];
   for i=1:nframe
-    filelist = [filelist ' ' strcat(fname_line,'_f',num2str(i,'%04u'),'.jpg ')];
+    filelist = [filelist ' ' fullfile(src_dir,strcat(fname_line,'_f',num2str(i,'%04u'),'.jpg '))];
   end
 
-  mkdir(fullfile(src_dir,fname_line));
-  command = ['mv ' filelist ' ' fname_line '/'];
+  %mkdir(fullfile(src_dir,fname_line));
+  command = ['mv ' filelist ' ' fullfile(src_dir,fname_line) '/'];
   [status, cmdout] = system(command);
   fprintf('%s: status(%d)\n', fname_line, status);
 
