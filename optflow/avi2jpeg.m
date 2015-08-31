dab7f413c8fca75710978d317b962dfea4aa4c46
Eunbyung Park <silverbottlep@gmail.com>
2015-08-31 15:23:56 -0400

    minor change

--- optflow/avi2jpeg.m
+++ optflow/avi2jpeg.m
@@ -1,16 +1,17 @@
-src_dir = '/home/eunbyung/Works/data/ucf101';
-dst_dir = '/home/eunbyung/Works/data/ucf101_jpeg';
+%src_dir = '/home/eunbyung/Works/data/ucf101';
+src_dir = '/home/eunbyung/Works/data/hmdb51/hmdb51_video';
+dst_dir = '/home/eunbyung/Works/data/hmdb51/hmdb51_image';
 
-split1_train = '/home/eunbyung/Works/data/ucfTrainTestlist/trainlist01.txt'
-split1_train = '/home/eunbyung/Works/data/ucfTrainTestlist/trainlist01.txt'
-split1_test = '/home/eunbyung/Works/data/ucfTrainTestlist/testlist01.txt'
+%split1_train = '/home/eunbyung/Works/data/ucfTrainTestlist/trainlist01.txt'
+%split1_train = '/home/eunbyung/Works/data/ucfTrainTestlist/trainlist01.txt'
+%split1_test = '/home/eunbyung/Works/data/ucfTrainTestlist/testlist01.txt'
 %train_strings = fileread(split1_train);
 
 WIDTH=340;
 HEIGHT=256;
 src_list = dir(src_dir);
 src_list = src_list(3:end);
-parfor i=1:length(src_list)
+for i=1:length(src_list)
   obj = VideoReader(fullfile(src_dir,src_list(i).name));
   k=0;
   while obj.hasFrame
@@ -18,7 +19,9 @@ parfor i=1:length(src_list)
     k=k+1;
     jpeg_name = strcat(src_list(i).name(1:end-4),'_f',num2str(k,'%04u'),'.jpg');
     cur_frame = imresize(cur_frame,[HEIGHT,WIDTH]);
-    imwrite(cur_frame, fullfile(dst_dir,jpeg_name),'jpg');
+    fprintf('%s\n',fullfile(dst_dir,jpeg_name));
+    %imwrite(cur_frame, fullfile(dst_dir,jpeg_name),'jpg');
   end
   fprintf('(%d:%d)',i,length(src_list));
+	pause;
 end
--- optflow/read_video.m
+++
@@ -1,6 +0,0 @@
-filename = '/home/eunbyung/Works/data/ucf101/v_PlayingDhol_g01_c01.avi';
-
-obj = VideoReader(filename);
-%video = get(obj);
-%mov = aviread(filename);
-
